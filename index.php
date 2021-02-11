ffd4a78f91ba32fc59ae7564f8b080c287f79b8f
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2021-02-11 15:18:49 -0300

    vision added

--- index.php
+++ index.php
@@ -12,26 +12,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>PHP WORK</title>
+    <title>Other projects</title>
 </head>
 
 <body class="bg-dark">
-    <h1 class="display-6 title text-center my-4">Others using PHP</h1>
+    <h1 class="display-6 title text-center my-4">Others projects</h1>
 
     <div class="links">
         <div class="row justify-content-center">
                 <div class="app mx-5 my-1 p-3 rounded text-center shadow">
                     <a href="my-wallet/index.php" target="_blank" ><img src="my-wallet/resources/logo.png"></a>
                      <p class="p-2">MY WALLET</p>
+                     <p>(PHP)</p>
                  </div>
                  <div class="app mx-5 my-1 p-3 rounded text-center shadow">
                      <a href="sender/index.php" target="_blank" ><img src="sender/assets/img/mail.jpg"></a>
                      <p class="p-2">SENDER</p>
+                     <p>(PHP)</p>
                  </div>
                  <div class="app mx-5 my-1 p-3 rounded text-center shadow">
                      <a href="stadistics/index.php" target="_blank" ><img src="stadistics/resources/ico.png" srcset=""></a>
                      <p class="p-2">STADISTICS</p>
-                 </div>        
+                     <p>(PHP)</p>
+                 </div>       
+                 <div class="app mx-5 my-1 p-3 rounded text-center shadow">
+                     <a href="https://vision456.herokuapp.com/" target="_blank" ><img class="pt-5 pb-4" src="vision-logo.png" srcset=""></a>
+                     <p class="p-2">VISION</p>
+                     <p>(NodeJS)</p>
+                 </div>     
         </div>
     </div>
 
@@ -41,6 +49,7 @@
             <li><p>'My wallet' and 'Sender' are kind of responsive, 'Stadistics' is not responsive at all...</p></li>
             <li><p>'My wallet' is one of the first so, it has no AJAX, that's why you are going to find many files in it.</p></li>
             <li><p>For simplicity to upload the site, the three are connected to the same database (it is all messed up) But they had their own DB at the beginning.</p></li>
+            <li><p>Vision was just an attempt to use only bootstrap to style it and i used the template to try a simple node server.</p></li>
             <li><p>Source code:  <a href="https://github.com/estoquin/php_work.git" target="_blank">https://github.com/estoquin/php_work.git</a></p></li>
             <li><p style="color:red;">'Sender' is not gonna work because of the server where it is hosted, i am having troubles setting up the mailer...</p></li>
         </ul>
